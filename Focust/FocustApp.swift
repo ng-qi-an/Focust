acf10b4b531301cf4697fc6fcc3d55b29dceaec0
Pop Plays <85472155+popQA17@users.noreply.github.com>
2023-11-12 23:31:47 +0800

    Initial Commit

---
+++ Focust/FocustApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocustApp.swift
+//  Focust
+//
+//  Created by Ng Qi An on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FocustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
