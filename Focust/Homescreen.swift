89aae13b3895c8ec66f7f3bf50f7e45683c5a894
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-13 20:13:17 +0800

    123456

---
+++ Focust/Homescreen.swift
@@ -0,0 +1,18 @@
+//
+//  Homescreen.swift
+//  Focust
+//
+//  Created by Ethan Chow on 13/11/23.
+//
+
+import SwiftUI
+
+struct Homescreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homescreen()
+}
