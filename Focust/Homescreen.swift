2a8bdf2694d338c1a7dd31d6d5b6f07cc368854a
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-14 18:18:51 +0800

    finished homepage?

--- Focust/Homescreen.swift
+++ Focust/Homescreen.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct Homescreen: View {
+    
+    @State var value = 0
     var body: some View {
         ZStack {
-            Color(red: 45/255, green: 212/255, blue: 191/255).edgesIgnoringSafeArea(.all)
-            Ellipse().frame(width: 450, height: 118).offset(y: -210).foregroundColor(.white).shadow(color: .white, radius: 5, y: -5)
+            Color(red: 45/255, green: 212/255, blue: 191/255)
+                .edgesIgnoringSafeArea(.all)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(88))
+                .offset(x: -100, y: -270)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(-60))
+                .offset(x: -170, y: -360)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(81))
+                .offset(x: -50, y: -380)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(-11))
+                .offset(x: 80, y: -340)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(151))
+                .offset(x: 150, y: -260)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Image(systemName: "triangle.fill")
+                .font(.system(size: 30))
+                .rotationEffect(.degrees(-151))
+                .offset(x: 150, y: -400)
+                .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                .shadow(color: .white, radius: 2)
+            Ellipse()
+                .frame(width: 395, height: 50)
+                .offset(y: -210)
+                .foregroundColor(.white)
+                .shadow(color: .white, radius: 5, y: -5)
             VStack {
-                Rectangle().frame(height: 650).offset(y: 114.0).foregroundColor(.white)
+                Text("**1**")
+                    .foregroundColor(.white)
+                    .font(.system(size: 60))
+                Text("hours focused")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                Rectangle()
+                    .frame(height: 650)
+                    .offset(y: 60)
+                    .foregroundColor(.white)
             }
             VStack {
                 HStack {
@@ -75,8 +127,16 @@ struct Homescreen: View {
                     Text("🏁")
                         .font(.system(size: 15))
                     ZStack {
-                        Rectangle().frame(width: 270, height: 10).cornerRadius(20).foregroundColor(Color(red: 242/255, green: 242/255, blue: 242/255)).shadow(color: Color(.lightGray), radius: 2, x: 0, y: 1)
-                        Rectangle().frame(width: 90, height: 10).cornerRadius(20).foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255)).offset(x: -90)
+                        Rectangle()
+                            .frame(width: 270, height: 10)
+                            .cornerRadius(20)
+                            .foregroundColor(Color(red: 242/255, green: 242/255, blue: 242/255))
+                            .shadow(color: Color(.lightGray), radius: 2, x: 0, y: 1)
+                        Rectangle()
+                            .frame(width: 90, height: 10)
+                            .cornerRadius(20)
+                            .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                            .offset(x: -90)
                     }
                     Text("🎉")
                         .font(.system(size: 15))
@@ -84,12 +144,241 @@ struct Homescreen: View {
                 Text("[ ] hrs left")
                     .opacity(0.3)
                     .offset(y: -90)
-                VStack {
-                    Text("**Your activity**")
-                        .opacity(0.5)
-                        .offset(x: -120)
-                    Rectangle().frame(width: 350, height: 271).foregroundColor(Color(red: 242/255, green: 242/255, blue: 242/255)).cornerRadius(15)
-                }.offset(y: -60)
+                Text("**Your activity**")
+                    .opacity(0.5)
+                    .offset(x: -120)
+                ZStack {
+                    Rectangle()
+                        .frame(width: 350, height: 290)
+                        .foregroundColor(Color(red: 242/255, green: 242/255, blue: 242/255))
+                        .cornerRadius(15)
+                    Image("flame")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 150)
+                        .offset(y: -65)
+                    Text("[ ]")
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .offset(y: -50)
+                    Text("**Day Streak**")
+                        .font(.system(size: 20))
+                        .opacity(0.6)
+                        .offset(y: 10)
+                    VStack {
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [
+                                    Color(red: 250/255, green: 204/255, blue: 21/255),
+                                    Color(red: 249/255, green: 115/255, blue: 22/255)
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .mask(Image(systemName: "trophy.fill"))
+                        .frame(width: 26, height: 28)
+                        .offset(x: 120, y: 85)
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [
+                                    Color(red: 250/255, green: 204/255, blue: 21/255),
+                                    Color(red: 249/255, green: 115/255, blue: 22/255)
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .mask(Image(systemName: "flag.checkered"))
+                        .frame(width: 26, height: 28)
+                        .offset(x: -120, y: 50)
+                        
+                        VStack {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 230, height: 3)
+                                    .foregroundColor(Color(red: 217/255, green: 217/255, blue: 217/255))
+                                    .offset(y: 70)
+                                ZStack {
+                                    LinearGradient(
+                                        gradient: Gradient(
+                                            colors: [
+                                                Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                Color(red: 249/255, green: 115/255, blue: 22/255)
+                                            ]
+                                        ),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                    .mask(Circle())
+                                    .frame(width: 15, height: 15)
+                                    Circle()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.white)
+                                        .opacity(1)
+                                }.offset(x: -120, y: 70)
+                                ZStack {
+                                    LinearGradient(
+                                        gradient: Gradient(
+                                            colors: [
+                                                Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                Color(red: 249/255, green: 115/255, blue: 22/255)
+                                            ]
+                                        ),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                    .mask(Circle())
+                                    .frame(width: 15, height: 15)
+                                    Circle()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.white)
+                                        .opacity(1)
+                                }.offset(x: -80, y: 70)
+                                ZStack {
+                                    LinearGradient(
+                                        gradient: Gradient(
+                                            colors: [
+                                                Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                Color(red: 249/255, green: 115/255, blue: 22/255)
+                                            ]
+                                        ),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                    .mask(Circle())
+                                    .frame(width: 15, height: 15)
+                                    Circle()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.white)
+                                        .opacity(1)
+                                }.offset(x: -40, y: 70)
+                                ZStack {
+                                    LinearGradient(
+                                        gradient: Gradient(
+                                            colors: [
+                                                Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                Color(red: 249/255, green: 115/255, blue: 22/255)
+                                            ]
+                                        ),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                    .mask(Circle())
+                                    .frame(width: 15, height: 15)
+                                    Circle()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.white)
+                                        .opacity(1)
+                                }.offset(y: 70)
+                                ZStack {
+                                    LinearGradient(
+                                        gradient: Gradient(
+                                            colors: [
+                                                Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                Color(red: 249/255, green: 115/255, blue: 22/255)
+                                            ]
+                                        ),
+                                        startPoint: .top,
+                                        endPoint: .bottom
+                                    )
+                                    .mask(Circle())
+                                    .frame(width: 15, height: 15)
+                                    Circle()
+                                        .frame(width: 10, height: 10)
+                                        .foregroundColor(.white)
+                                        .opacity(0)
+                                }.offset(x: 40, y: 70)
+                                ZStack {
+                                    if value == 0 {
+                                        Circle()
+                                            .frame(width: 15, height: 15)
+                                            .foregroundColor(.gray)
+                                        Circle()
+                                            .frame(width: 10, height: 10)
+                                            .foregroundColor(.white)
+                                            .opacity(1)
+                                    } else {
+                                        LinearGradient(
+                                            gradient: Gradient(
+                                                colors: [
+                                                    Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                    Color(red: 249/255, green: 115/255, blue: 22/255)
+                                                ]
+                                            ),
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                        )
+                                        .mask(Circle())
+                                        .frame(width: 15, height: 15)
+                                        Circle()
+                                            .frame(width: 10, height: 10)
+                                            .foregroundColor(.white)
+                                            .opacity(1)
+                                    }
+                                }.offset(x: 80, y: 70)
+                                ZStack {
+                                    if value == 0 {
+                                        Circle()
+                                            .frame(width: 15, height: 15)
+                                            .foregroundColor(.gray)
+                                        Circle()
+                                            .frame(width: 10, height: 10)
+                                            .foregroundColor(.white)
+                                            .opacity(1)
+                                    } else {
+                                        LinearGradient(
+                                            gradient: Gradient(
+                                                colors: [
+                                                    Color(red: 250/255, green: 204/255, blue: 21/255),
+                                                    Color(red: 249/255, green: 115/255, blue: 22/255)
+                                                ]
+                                            ),
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                        )
+                                        .mask(Circle())
+                                        .frame(width: 15, height: 15)
+                                        Circle()
+                                            .frame(width: 10, height: 10)
+                                            .foregroundColor(.white)
+                                            .opacity(1)
+                                    }
+                                }.offset(x: 120, y: 70)
+                            }.offset(y: -20)
+                            HStack {
+                                Text("M")
+                                    .font(.system(size: 15))
+                                    .offset(x: -65, y: 90)
+                                    .opacity(0.5)
+                                Text("T")
+                                    .font(.system(size: 15))
+                                    .offset(x: -45, y: 90)
+                                    .opacity(0.5)
+                                Text("W")
+                                    .font(.system(size: 15))
+                                    .offset(x: -25, y: 90)
+                                    .opacity(0.5)
+                                Text("T")
+                                    .font(.system(size: 15))
+                                    .offset(x: -5, y: 90)
+                                    .opacity(0.5)
+                                Text("F")
+                                    .font(.system(size: 15))
+                                    .offset(x: 17, y: 90)
+                                    .opacity(1)
+                                Text("S")
+                                    .font(.system(size: 15))
+                                    .offset(x: 42, y: 90)
+                                    .opacity(0.5)
+                                Text("S")
+                                    .font(.system(size: 15))
+                                    .offset(x: 65, y: 90)
+                                    .opacity(0.5)
+                            }.offset(y: -40)
+                        }
+                    }
+                }.offset(y: -70)
                 HStack {
                     TabView {
                         VStack {
@@ -134,8 +423,8 @@ struct Homescreen: View {
                         }.offset(x: -10)
                     }
                     .tabViewStyle(PageTabViewStyle())
-                }.offset(y: -150)
-            }.offset(y: 300)
+                }.offset(y: -180)
+            }.offset(y: 315)
         }
     }
 }
