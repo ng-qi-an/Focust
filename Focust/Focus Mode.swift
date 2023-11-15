aa65f7ceda3d12051285609318724fbc609329f2
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-15 15:38:14 +0800

    changes

---
+++ Focust/Focus Mode.swift
@@ -0,0 +1,18 @@
+//
+//  Focus Mode.swift
+//  Focust
+//
+//  Created by Ethan Chow on 15/11/23.
+//
+
+import SwiftUI
+
+struct Focus_Mode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Focus_Mode()
+}
