136328c6a58beb8801afbb1298d49f39420e10a2
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-16 13:34:08 +0800

    login and starting page

---
+++ Focust/AuthStartingPage.swift
@@ -0,0 +1,18 @@
+//
+//  AuthStartingPage.swift
+//  Focust
+//
+//  Created by Ethan Chow on 16/11/23.
+//
+
+import SwiftUI
+
+struct AuthStartingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthStartingPage()
+}
---
+++ Focust/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Focust
+//
+//  Created by Ethan Chow on 16/11/23.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
