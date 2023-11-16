f0505f23c3e5b5ce0d0ff8a1cd8bab1256865fb2
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-16 13:34:18 +0800

    code

--- Focust/AuthStartingPage.swift
+++ Focust/AuthStartingPage.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct AuthStartingPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 216/255, green: 255/255, blue:250/255)
+                .ignoresSafeArea(.all)
+            Image("startPageBg")
+                .resizable()
+                .frame(width: 394, height: 456)
+                .offset(y: 185)
+            VStack(spacing: 20){
+                VStack(spacing: 5) {
+                    Image("placeholderIcon")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .offset(y: -30)
+                    Text("**Start you focus journey with us**")
+                        .font(.system(size: 25))
+                        .foregroundColor(Color(red: 102/255, green: 100/255, blue: 100/255))
+                    Text("**TODAY!**")
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(red: 45/255, green: 212/255, blue: 191/255))
+                }.offset(y: -125)
+                
+                Button {
+                    
+                } label : {
+                    HStack {
+                        Text("Sign Up")
+                        Image(systemName: "person.crop.circle.badge.plus")
+                            .padding(.leading, 150)
+                    }
+                    .frame(width: 315, height: 50)
+                    .foregroundColor(.black)
+                    .background(.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color(red: 218/255, green: 218/255, blue: 218/255), radius: 4, y: 3)
+                }
+                Button {
+                    
+                } label : {
+                    HStack {
+                        Text("Log In ")
+                        Image(systemName: "key")
+                            .padding(.leading, 150)
+                    }
+                    .frame(width: 315, height: 50)
+                    .foregroundColor(.black)
+                    .background(.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color(red: 218/255, green: 218/255, blue: 218/255), radius: 4, y: 3)
+                }
+            }
+        }
     }
 }
 
--- Focust/Login.swift
+++ Focust/Login.swift
@@ -6,10 +6,84 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Login: View {
+    @State private var phoneNo = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color(red: 216/255, green: 255/255, blue:250/255)
+                    .ignoresSafeArea(.all)
+                Image("authBg")
+                    .resizable()
+                    .frame(width: 394, height: 253)
+                    .offset(y: -312)
+                VStack {
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .frame(width: 19, height: 30)
+                        .foregroundColor(.white)
+                        .offset(x: -150, y: -245)
+                    HStack {
+                        Text("**Login**")
+                            .font(.system(size: 20))
+                    }
+                    .frame(width: 220, height: 70)
+                    .background(.white)
+                    .cornerRadius(15)
+                    .offset(y: -240)
+                    VStack(spacing: 20){
+                        HStack {
+                            Image(systemName: "phone")
+                                .padding(.leading, 30)
+                            TextField("Phone Number", text: $phoneNo)
+                                .keyboardType(.numberPad)
+                                .padding(.leading, 10)
+                                .onChange(of: phoneNo) { newValue in
+                                    if newValue.count > 8 {
+                                        phoneNo = String(newValue.prefix(8))
+                                    }
+                                }
+                        }
+                        .frame(width: 350, height: 50)
+                        .background(.white)
+                        .cornerRadius(20)
+                        .shadow(color: Color(red: 218/255, green: 218/255, blue: 218/255), radius: 4, y: 3)
+                        HStack {
+                            Image(systemName: "lock")
+                                .padding(.leading, 30)
+                            SecureField("Password", text: $password)
+                                .padding(.leading, 10)
+                        }
+                        .frame(width: 350, height: 50)
+                        .background(.white)
+                        .cornerRadius(20)
+                        .shadow(color: Color(red: 218/255, green: 218/255, blue: 218/255), radius: 4, y: 3)
+                    }.offset(y: -50)
+                    Button {
+                        
+                    } label : {
+                        HStack {
+                            Text("Login")
+                                .padding(.leading, 30)
+                                .foregroundColor(.white)
+                            Spacer()
+                            Image(systemName: "paperplane.fill")
+                                .padding(.trailing, 30)
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 350, height: 60)
+                        .background(Color(red: 45/255, green: 212/255, blue: 191/255))
+                        .cornerRadius(20)
+                        .shadow(color: Color(red: 218/255, green: 218/255, blue: 218/255), radius: 4, y: 3)
+                        .offset(y: -20)
+                    }
+                }
+            }
+        }
     }
 }
 
