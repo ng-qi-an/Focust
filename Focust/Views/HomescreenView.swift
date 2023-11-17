9fad0785b5f47706a5e98af090160ef235691cb9
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-17 10:18:32 +0800

    @money

--- Focust/Views/HomescreenView.swift
+++ Focust/Views/HomescreenView.swift
@@ -52,19 +52,11 @@ struct HomescreenView: View {
                 .offset(x: 150, y: -400)
                 .foregroundColor(.white)
                 .opacity(0.2) //rando triangles
-            if account.isDarkMode == false {
                 Ellipse()
                     .frame(maxWidth: .infinity, maxHeight: 50)
                     .offset(y: -200)
-                    .foregroundColor(.white)
-                    .shadow(color: .white, radius: 5, y: -5) //bg oval
-            } else {
-                Ellipse()
-                    .frame(maxWidth: .infinity, maxHeight: 50)
-                    .offset(y: -200)
-                    .foregroundColor(.black)
-                    .shadow(color: .black, radius: 5, y: -5) //bg oval
-            }
+                    .foregroundColor(account.isDarkMode ? .black : .white)
+                    .shadow(color: account.isDarkMode ? .black : .white, radius: 5, y: -5) //bg oval
             VStack {
                 Text("**1**")
                     .foregroundColor(.white)
@@ -72,17 +64,10 @@ struct HomescreenView: View {
                 Text("hours focused")
                     .foregroundColor(.white)
                     .font(.system(size: 20)) //number of hours focused
-                if account.isDarkMode == false {
                     Rectangle()
                         .frame(height: 650)
                         .offset(y: 50)
-                        .foregroundColor(.white) //bg rectangle for dark mode
-                } else {
-                    Rectangle()
-                        .frame(height: 650)
-                        .offset(y: 50)
-                        .foregroundColor(.black) //bg rectangle for dark mode
-                }
+                        .foregroundColor(account.isDarkMode ? .black : .white) //bg rectangle for dark mode
                 
             }.offset(y: 20)
             VStack {
