9f6f46d36a6ed143af1e3be1e8e9ba4245a06f0a
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-11-20 16:02:41 +0800

    @mwoney

---
+++ Focust/Views/Focus Mode/Focus_ModeTypeSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  Focus_ModeTypeSelectionView.swift
+//  Focust
+//
+//  Created by Ethan Chow on 20/11/23.
+//
+
+import SwiftUI
+
+struct Focus_ModeTypeSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Focus_ModeTypeSelectionView()
+}
